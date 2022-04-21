ac7d473916745fdf9875b2d7e4c37bdffb9d93a9
Omkar2102 <omkar.narkar2102@gmail.com>
2022-04-21 02:48:05 -0400

    commit 4(2)

--- org.jhotdraw8.draw/src/main/java/org.jhotdraw8.draw/org/jhotdraw8/svg/io/AbstractFXSvgWriter.java
+++ org.jhotdraw8.draw/src/main/java/org.jhotdraw8.draw/org/jhotdraw8/svg/io/AbstractFXSvgWriter.java
@@ -1103,7 +1103,7 @@ public abstract class AbstractFXSvgWriter extends AbstractPropertyBean implement
     private void writeRegionStartElement(@NonNull XMLStreamWriter w, @NonNull Region region) throws IOException, XMLStreamException {
         w.writeStartElement("g");
     }
-
+// TODO:These nested if statements could be combined FIXME: This method needs to combine the nested if statements at line 1186
     private void writeRegionChildElements(@NonNull XMLStreamWriter w, @NonNull Region region) throws IOException, XMLStreamException {
 
         double x = region.getLayoutX();
